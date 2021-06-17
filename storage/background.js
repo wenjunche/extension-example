98e427d2839a6955a99b4be55da125c98915ea67
wenjunche <wenjun@openfin.co>
2021-06-16 22:13:11 -0400

    storage

--- storage/background.js
+++ storage/background.js
@@ -1,15 +1,23 @@
-const now = new Date();
-const key = 'time';
+const now = Date.now();
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-  console.log(`storage changed ${changes} ${namespace}`);
+  console.log(`storage changed ${JSON.stringify(changes)} ${JSON.stringify(namespace)}`);
 });
 
-chrome.storage.sync.get([key], function(result) {
+chrome.storage.sync.get('time', function(result) {
   console.log(`Value currently is ${JSON.stringify(result)}`);
 
-  chrome.storage.sync.set({key: now}, function() {
+  chrome.storage.sync.set({'time': now}, function() {
     console.log('Value is set to ', now);
+
+
+    chrome.storage.sync.get('time', function(check) {
+      console.log(`check value is ${JSON.stringify(check)}`);
+    });    
   });
   
+});
+
+chrome.storage.sync.get(null, function(result) {
+  console.log(`All Values are ${JSON.stringify(result)}`);
 });
