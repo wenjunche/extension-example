bc7576ba98244e9dcd9b25004d3e28606563703c
Wenjun Che <wenjun@here.io>
2025-04-04 16:53:49 -0400

    updates

--- web-navigation/background.js
+++ web-navigation/background.js
@@ -1,6 +1,22 @@
 
 
 const ebUrl = 'http://localhost/login';
+const ebBaseUrl = 'http://localhost';
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log(`onInstalled`);
+  /*
+  chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [{
+          id: 1,
+          priority: 1,
+          action: { type: "redirect",  redirect: { url: "fins://qa.is.here.io/platform/api/platform.json?$$launchcontentuuid=186e4e82-f7b2-4ce9-ab6f-9ce05611ad7f" } },
+          condition: { urlFilter: "*://*.quora.com/*", resourceTypes: ["script", "main_frame"] }
+      }],
+      removeRuleIds: [1]
+  });
+  */
+});
 
 chrome.webNavigation.onBeforeNavigate.addListener(details => {
   console.log(`onBeforeNavigate ${JSON.stringify(details)}`);
@@ -18,3 +34,39 @@ chrome.webNavigation.onCommitted.addListener(details => {
 chrome.webNavigation.onCompleted.addListener(details => {
   console.log(`onCompleted ${JSON.stringify(details)}`);
 });
+
+
+async function getApps() {
+  const resp = await fetch(`${ebBaseUrl}/platform/user/apps`);
+  if (resp.status === 200) {
+    const apps = await resp.json();
+    console.log(apps);
+    const rules = [];
+    for (const app of apps) {
+      rules.push(
+        {
+          id: 1,
+          priority: 1,
+          action: {
+            type: "redirect",
+            redirect: { url: "https://new-url.com" }
+          },
+          condition: {
+            urlFilter: "example.com/old",
+            resourceTypes: ["main_frame"]
+          }
+        },        
+      );
+    }    
+  } else {
+    chrome.tabs.create({ url: `${ebBaseUrl}/login`});
+
+  }
+
+  // chrome.windows.create({
+  //   url: "popup.html",
+  //   type: "popup",
+  //   width: 400,
+  //   height: 500
+  // });
+}
