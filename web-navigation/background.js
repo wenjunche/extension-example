9d9b495de11a16e828fc265cc4a6707b8863c390
Wenjun Che <wenjun@here.io>
2025-04-04 14:56:08 -0400

    updates

--- web-navigation/background.js
+++ web-navigation/background.js
@@ -1,8 +1,14 @@
 
 
+const ebUrl = 'http://localhost/login';
 
 chrome.webNavigation.onBeforeNavigate.addListener(details => {
   console.log(`onBeforeNavigate ${JSON.stringify(details)}`);
+  const isMainFrame = detail?.frameType === 'outermost_frame';
+  if (isMainFrame && details.url === 'https://www.quora.com/') {
+        // Instead of blocking, update the tab's URL
+        chrome.tabs.update(details.tabId, { url: ebUrl });
+  }
 });
 
 chrome.webNavigation.onCommitted.addListener(details => {
--- web-request/background.js
+++ web-request/background.js
@@ -1,17 +1,63 @@
 const urls= ['https://*/*', 'http://*/*'];
 const infoSpec = ['blocking'];
 
+const ebUrl = 'fins://qa.is.here.io/platform/api/platform.json?$$launchcontentuuid=186e4e82-f7b2-4ce9-ab6f-9ce05611ad7f';
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log(`onInstalled`);
+  /*
+  chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [{
+          id: 1,
+          priority: 1,
+          action: { type: "redirect",  redirect: { url: "fins://qa.is.here.io/platform/api/platform.json?$$launchcontentuuid=186e4e82-f7b2-4ce9-ab6f-9ce05611ad7f" } },
+          condition: { urlFilter: "*://*.quora.com/*", resourceTypes: ["script", "main_frame"] }
+      }],
+      removeRuleIds: [1]
+  });
+  */
+});
+
+chrome.webRequest.onBeforeRequest.addListener(
+  async (details) => {
+    console.log(`onBeforeRequest ${details.url}`);
+    if (details.url === 'https://www.quora.com/') {
+        console.log(`Redirecting ${details.url} to ${ebUrl}`);
+        // Instead of blocking, update the tab's URL
+        // chrome.tabs.update(details.tabId, { url: ebUrl });
+        login();
+        return; // Prevent further processing
+    }
+  },
+  { urls: ["<all_urls>"] }
+  // ["requestBody"] // No blocking supported in non-enterprise installs
+);
+
+function login() {
+  // chrome.tabs.create({ url: 'http://localhost/login'});
+  chrome.windows.create({
+    url: "popup.html",
+    type: "popup",
+    width: 400,
+    height: 500
+});  
+}
+
+/* v2
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     console.log(`onBeforeRequest ${details.url}`);
     if (details.url === 'https://www.quora.com/') {
       return {cancel: true};
     }
+    if (details.url === 'https://www.bbc.com/') {
+      return { 'redirectUrl': 'fins://qa.is.here.io/platform/api/platform.json?$$launchcontentuuid=186e4e82-f7b2-4ce9-ab6f-9ce05611ad7f' }
+    }
   },
   {urls},
   infoSpec
 );
-
+*/
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
     console.log(`onBeforeSendHeaders ${details.url}`);
@@ -55,3 +101,4 @@ chrome.webRequest.onErrorOccurred.addListener(
   },
   {urls}
 );
+
