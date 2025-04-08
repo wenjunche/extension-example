5b51631db110439f902bda679e199e352f7365e7
Wenjun Che <wenjun@here.io>
2025-04-08 16:18:07 -0400

    add webNavigation example

--- web-navigation/background.js
+++ web-navigation/background.js
@@ -5,6 +5,8 @@ const ebUrl = 'http://localhost/login';
 //const ebFinBaseUrl = 'fin://localhost';
 let ebEnv;
 
+const domainMap = new Map();  // domain => fins link for the  content UUID
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log(`onInstalled`);
 
@@ -23,26 +25,16 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 
   init();
-
-  /*
-  chrome.declarativeNetRequest.updateDynamicRules({
-      addRules: [{
-          id: 1,
-          priority: 1,
-          action: { type: "redirect",  redirect: { url: "fins://qa.is.here.io/platform/api/platform.json?$$launchcontentuuid=186e4e82-f7b2-4ce9-ab6f-9ce05611ad7f" } },
-          condition: { urlFilter: "*://*.quora.com/*", resourceTypes: ["script", "main_frame"] }
-      }],
-      removeRuleIds: [1]
-  });
-  */
 });
 
 chrome.webNavigation.onBeforeNavigate.addListener(details => {
   console.log(`onBeforeNavigate ${JSON.stringify(details)}`);
   const isMainFrame = details?.frameType === 'outermost_frame';
-  if (isMainFrame && details.url === 'https://www.quora.com/') {
-        // Instead of blocking, update the tab's URL
-        chrome.tabs.update(details.tabId, { url: ebUrl });
+  const redirect = checkUrlWithDomainMap(details.url);
+  if (domainMap.size > 0 && redirect) {
+      console.debug(`redirect ${details.url} to ${redirect}`);
+      const url = chrome.runtime.getURL(redirect);
+      chrome.tabs.update(details.tabId, { url });
   }
 });
 
@@ -88,7 +80,7 @@ async function updateAllRules() {
             action: {
               type: 'redirect',
 //              redirect: { url: `${ebFinBaseUrl}/platform/api/platform.json?$$launchcontentuuid=${app.contentId}`}
-              redirect: { extensionPath: `/redirect.html?source=${domain}&target=${url}` }
+              redirect: { extensionPath: `/redirect.html?source=${encodeURIComponent(domain)}&target=${encodeURIComponent(url)}` }
             },
             condition: {
               urlFilter: `||${domain}`,
@@ -107,9 +99,47 @@ async function updateAllRules() {
   }
 }
 
+async function updateDomainMap() {
+  console.debug('updateDomainMap');
+  const ebBaseUrl = `${ebEnv.startsWith('localhost')?'http':'https'}://${ebEnv}`;
+  const ebFinBaseUrl = `${ebEnv.startsWith('localhost')?'fin':'fins'}://${ebEnv}`;
+  const resp = await fetch(`${ebBaseUrl}/platform/api/user/apps`);
+  if (resp.status === 200) {
+    const apps = await resp.json();
+    console.log(apps);
+    domainMap.clear();
+    for (const app of apps) {
+      for (const domain of app.domains) {
+        const url = `${ebFinBaseUrl}/platform/api/platform.json?$$launchcontentuuid=${app.contentId}`;
+        const redirect = `/redirect.html?source=${encodeURIComponent(domain)}&target=${encodeURIComponent(url)}`
+        domainMap.set(domain, redirect);
+      }
+    }
+    console.debug('new domain map', domainMap);
+  } else {
+    console.debug('getApps rejected, trying to log in', resp.status);
+    chrome.tabs.create({ url: `${ebBaseUrl}/login`});
+  }
+}
+
+function checkUrlWithDomainMap(url) {
+  for (const [domain, redirect] of domainMap.entries()) {
+    if (url.includes(domain)) {  // domain matching. good enough for POC
+        console.debug(`${url} matched ${domain}`);
+        return redirect;
+    }
+  }
+}
+
+// call these if use declarativeNetRequest
 async function resetAllRules() {
   await clearAllRules();
   await updateAllRules();
+}
+
+// call this if use webNavigation to redirect
+async function resetAllDomainMap() {
+  await updateDomainMap();
 }
 
 async function getEBEnv() {
@@ -126,7 +156,8 @@ async function init() {
   if (env) {
     console.debug('found EB env', env);
     ebEnv = env;
-    resetAllRules();
+    // resetAllRules();      // call this if testing with declarativeNetRequest
+    resetAllDomainMap();  // call this if testing with webNavigation
   } else {
     chrome.windows.create({
        url: "popup.html",
