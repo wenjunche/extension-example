6148ac63139d13c3bd207190b9856d38aa170a1e
Wenjun Che <wenjun@here.io>
2025-05-27 17:16:30 -0400

    schema test

---
+++ schema-test/background.js
@@ -0,0 +1,13 @@
+chrome.storage.managed.get(['testValue'], function(result) {
+  if (chrome.runtime.lastError) {
+    console.error("Managed storage error:", chrome.runtime.lastError);
+  } else {
+    console.log("Managed testValue:", result.testValue);
+  }
+});
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace === 'managed' && changes.testValue) {
+    console.log('Managed testValue changed:', changes.testValue.newValue);
+  }
+});
